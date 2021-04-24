8c390bcfb68b9b9d46bb71da432d755c902e930b
mreguant <40800513+mreguant@users.noreply.github.com>
2021-04-24 00:38:46 -0500

    adding/fixing proper representation of uncertainty tree

--- code/model.jl
+++ code/model.jl
@@ -20,38 +20,106 @@ end
 
 
 # this function takes correlations in uncertainty and generates transition paths
-function uncertainty_model(df_clim::DataFrame, T::Int64, Y::Int64, D::Int64; damage="none", future="random")
+function uncertainty_model(df_clim::DataFrame, T::Int64, T1::Int64, Y::Int64, D::Int64; damage="none", uncertainty="none", climate="none", future="random", alpha=0.2, alphad=0.2)
 
-	df = DataFrame(p = Int64[], s = Int64[], s_1 = Int64[], ind_tr = Array{Int64,1}[], tr = Array{Float64,1}[], 
-		zeta = Float64[], g3 = Float64[], Y_last = Bool[]);
-
-    # year 0
+    ## damages
     g = [0.0, 0.032, 0.138];
     if (damage=="Nordhaus")
-        g = [g[1]]
-    end
-    if (damage=="Medium")
-        g = [g[2]]
-    end
-    if (damage=="Extreme")
-        g = [g[3]]
+        g = [0.0, 0.0, 0.0];
+    elseif (damage=="Medium")
+        g = [0.032, 0.032, 0.032];
+    elseif (damage=="Extreme")
+        g = [0.138, 0.138, 0.138];
     end
 
-    # start year
-    push!(df, [1, 1, 0, [Int(i) for i=2:1+D], [1.0/D for i=1:D], Y*mean(df_clim.Column1), 0.0, false])
-
-    # populating the future
-    if (future=="random")
-        ct = 1;
-        for p = 2:T
-            Dp = D^(p-1);   # draws accumulated this period
-            Dp_1 = D^(p-2); # draws previous period
-            for d = 1:D
-                push!(df, [p, ct+d, max(1+D*(p-3)+d,1), [Int(i) for i=2+D*(p-1):1+D*p], [1.0/D for i=1:D], Y*sample(df_clim.Column1), sample(g), (p==T)])
-                #push!(df, [p, 1+(p-2)*D+d, max(1+D*(p-3)+d,1), [Int(1+D*(p-1)+d)], [1.0], Y*sample(df_clim.Column1), sample(g), (p==T)])
-            end
-            ct = ct + D;
+    ## climate sensitivities (average over Y or just one draw for all Y)
+    if (uncertainty=="average")
+        df_clim[!,:mean] = [mean(sample(df_clim.Column1,Y)) for r in eachrow(df_clim)];
+        df_clim.sensitivity = df_clim.mean;
+    else
+        df_clim.sensitivity = df_clim.Column1;
+    end
+    climat = Array{Any}(nothing, T);
+    for p = 2:T
+        Dp = Int(D^min(p-1,T1));
+        climtemp = Float64[]
+        if (climate=="persistent")
+            climtemp = sort(sample(df_clim.sensitivity,Dp));
+        else
+            climtemp = sample(df_clim.sensitivity,Dp);
         end
+        climat[p] = climtemp;
+    end
+
+    ## building transition matrix
+    tr = Array{Any}(nothing, T);
+    ind = Array{Any}(nothing, T);
+    s1 = Array{Any}(nothing, T);
+    damage = Array{Any}(nothing, T);
+    ct = 1;
+    for p = 2:T
+        Dp = Int(D^min(p-1,T1));
+        Dp_1 = Int(max(D^min(p-2,T1),D)); # states in previous period        
+        trp  = Array{Any}(nothing, Dp);
+        indp = Array{Any}(nothing, Dp);
+        s1p  = Array{Any}(nothing, Dp);
+        dmgp = Array{Any}(nothing, Dp);
+        for dp=1:Dp
+            # transition to states
+            if (p <= T1)
+                d = Int(dp-floor((dp-1)/D)*D);
+                if (future=="random")
+                    transition = [1.0/D for i=1:D];
+                elseif (future=="gradual")
+                    denom = sum(exp((p-1)*alpha * (d==i)) for i in 1:D)  #+ (p-1)*alphad * (ceil(d/(D/3))==ceil(i/(D/3)))
+                    transition = [exp((p-1)*alpha * (d==i))/denom for i in 1:D];
+                end
+                startt = Int(ct + Dp + D*(dp-1) + 1);
+                endt = Int(ct + Dp + D*dp);
+                tindex = [Int(i) for i=startt:endt];
+                sindex = Int(max(ct - Dp_1 + ceil(dp/Dp_1),1));
+
+            else
+                transition = [1.0];
+                tindex = [Int(ct+Dp+dp)];
+                sindex = Int(max(ct-Dp_1+ceil(dp/(Dp/Dp_1)),1));
+            end
+            trp[dp] = transition;
+            indp[dp] = tindex;
+            s1p[dp]  = sindex;
+
+            # expected damage probabilities
+            if (future=="random")
+                transition = [1.0/3.0 for i=1:3];
+            elseif (future=="gradual")  # NOT WORKING YET
+                dg = Int(ceil(dp/(Dp/3))); 
+                denom = sum(exp((p-1)*alpha * (dg==i)) for i in 1:3)  #+ (p-1)*alphad * (ceil(d/(D/3))==ceil(i/(D/3)))
+                transition = [exp((p-1)*alpha * (dg==i))/denom for i in 1:3];
+            end
+            dmgp[dp] = transition;
+        end
+        tr[p] = trp;
+        ind[p] = indp;
+        s1[p] = s1p;
+        damage[p] = dmgp;
+        ct = ct + Dp;
+    end
+
+    ## populating data frame
+    df = DataFrame(p = Int64[], s = Int64[], s_1 = Int64[], ind_tr = Array{Int64,1}[], tr = Array{Float64,1}[], 
+        zeta = Float64[], g = Array{Float64,1}[],  pr_g = Array{Float64,1}[], Y_last = Bool[]);
+    
+    # year 0 (damages irrelevant, flat prior)
+    push!(df, [1, 1, 0, [Int(i) for i=2:1+D], [1.0/D for i=1:D], Y*mean(df_clim.Column1), g, [1.0/3, 1.0/3, 1.0/3], false]);
+    
+    # other years
+    ct = 1;
+    for p = 2:T
+        Dp = D^min(p-1,T1);
+        for d = 1:Dp
+            push!(df, [p, ct+d, s1[p][d,1], ind[p][d], tr[p][d], Y*climat[p][d], g, damage[p][d], (p==T)]);
+        end
+        ct = ct + Dp;
     end
 
 	return df;
@@ -81,7 +149,7 @@ function solve_model(pm::Dict{String,Float64}, df::DataFrame)
     @variable(model, K[1:S]);  # capital
     @variable(model, C[1:S] >= 0.0);  # consumption
     @variable(model, I[1:S] >= 0.0);  # investment
-    @variable(model, N[1:S] >= 1.0);  # damages
+    @variable(model, N[1:S,1:3] >= 1.0);  # damages
     @variable(model, 0.0 <= E[1:S] <= 60.0);  # energy (allow at most 60 Gt per year)
     @variable(model, Y[1:S] >= 1.0);  # temperature
     @variable(model, U[1:S]);  # utility function
@@ -98,8 +166,8 @@ function solve_model(pm::Dict{String,Float64}, df::DataFrame)
     # no borrowing at capital T
     @constraint(model, [s in S_1+1:S], I[s] >= 0.0);
 
-    # definition utility function
-    @NLconstraint(model, [s=1:S], U[s] == (C[s]/N[s])^(1-pm["eta"]) * E[s]^pm["eta"]);
+    # definition utility function integrating possible damages
+    @NLconstraint(model, [s=1:S], U[s] == sum(df.pr_g[s][i] * (C[s]/N[s,i])^(1-pm["eta"]) * E[s]^pm["eta"] for i in 1:3));
 
     # value function - Bellman
     @constraint(model, [s in S_1+1:S], V[s] == U[s]);
@@ -109,7 +177,7 @@ function solve_model(pm::Dict{String,Float64}, df::DataFrame)
     @constraint(model, [s=2:S], K[s] == I[df.s_1[s]] + (1.0-pm["delta"]) * K[df.s_1[s]]);
     @constraint(model, [s=1:S], C[s] + I[s] == pm["alpha"] * K[s]);
     @constraint(model, [s=2:S], Y[s] == Y[df.s_1[s]] + df.zeta[s] * E[df.s_1[s]]/3700.0);  # temperature equation
-    @NLconstraint(model, [s=1:S], log(N[s]) == pm["g1"] * Y[s] + pm["g2"] * Y[s]^2 + df.g3[s] * (Y[s]-Y[1])^3);  # damage equation
+    @NLconstraint(model, [s=1:S,i=1:3], log(N[s,i]) == pm["g1"] * Y[s] + pm["g2"] * Y[s]^2 / 2.0 + df.g[s][i] * (Y[s]-Y[1])^2);  # damage equation
 
     # constraint on last periods
     @constraint(model, [s=S_1+1:S], E[s] <= sum(E[spast] for spast in 1:S_1)/S_1);
@@ -127,7 +195,7 @@ function solve_model(pm::Dict{String,Float64}, df::DataFrame)
             I = Float64[], C = Float64[], E = Float64[], Y = Float64[], N = Float64[]);
         for s=1:S
             push!(results, [df.p[s], s, JuMP.value(U[s]), JuMP.value(V[s]), JuMP.value(K[s]),
-                JuMP.value(I[s]), JuMP.value(C[s]), JuMP.value(E[s]), JuMP.value(Y[s]), JuMP.value(N[s])])
+                JuMP.value(I[s]), JuMP.value(C[s]), JuMP.value(E[s]), JuMP.value(Y[s]), mean(1.0 ./JuMP.value(N[s]))])
         end
         return results;
 
@@ -140,7 +208,7 @@ function solve_model(pm::Dict{String,Float64}, df::DataFrame)
 end
 
 
-function run_model(T::Int64, Y::Int64, D::Int64, beta::Float64,  pm::Dict{String,Float64}, dmg::String)
+function run_model(T::Int64, T1::Int64, Y::Int64, D::Int64, beta::Float64, pm::Dict{String,Float64}; dmg="none", clim="none", fut="random", mc=20)
     
     params["beta"] = 1/((2.0-beta)^Y);
 
@@ -150,8 +218,10 @@ function run_model(T::Int64, Y::Int64, D::Int64, beta::Float64,  pm::Dict{String
     # run several draws
     results = DataFrame(p = Int64[], s = Int64[], U = Float64[], V = Float64[], K = Float64[],
             I = Float64[], C = Float64[], E = Float64[], Y = Float64[], N = Float64[]);
-    for d in 1:50
-        df = uncertainty_model(df_clim, T, Y, D, damage=dmg);
+
+    # averaging outcomes over several monte carlos
+    for d in 1:mc
+        df = uncertainty_model(df_clim, T, T1, Y, D, damage=dmg, climate=clim, future=fut);
         res = solve_model(params, df);
         append!(results, res);
     end
@@ -172,7 +242,7 @@ function plot_model(df_plt::DataFrame, ref_case::DataFrame; scat=true)
     if (scat==true)
         p1 = scatter(df_plt.p, df_plt.E/3.7, xlabel = "Emissions", ylabel = "Gt C") # Make a line plot
         p2 = scatter(df_plt.p, df_plt.Y, xlabel = "Temperature", ylabel = "degree C") # Make a scatter plot
-        p3 = scatter(df_plt.p, 1.0 ./df_plt.N, xlabel = "1/N")
+        p3 = scatter(df_plt.p, df_plt.N, xlabel = "1/N")
         p4 = scatter(df_plt.p, df_plt.I, xlabel = "Investment")
     else
         df_E = combine(groupby(df_plt,:p), :E=>mean, :Y=>mean, :N=>mean, :I=>mean, :C=>mean, :U=>mean);
@@ -181,7 +251,7 @@ function plot_model(df_plt::DataFrame, ref_case::DataFrame; scat=true)
         p1 = plot!(df_ref.p, df_ref.E_mean/3.7, xlabel = "Emissions", ylabel = "Gt C") # Make a line plot
         p2 = plot(df_E.p, df_E.Y_mean, xlabel = "Temperature", ylabel = "degree C") # Make a scatter plot
         p2 = plot!(df_ref.p, df_ref.Y_mean, xlabel = "Temperature", ylabel = "degree C") # Make a scatter plot
-        p3 = plot(df_E.p, 1.0 ./df_E.N_mean, xlabel = "1/N")
+        p3 = plot(df_E.p, df_E.N_mean, xlabel = "1/N")
         p4 = plot(df_E.p, df_E.U_mean./df_ref.U_mean, xlabel = "Rel. U to Case 1")
     end
         
--- code/run_model.jl
+++ code/run_model.jl
@@ -18,29 +18,46 @@ using Plots
 if ENV["USER"]=="mreguant"
     path = "/Users/mreguant/Documents/git/macro-annual-comment/"
 end
-include(string(path,"code/model.jl"));
 
 
 ## RUN model  --------------------------
 
-# Settings
+# include file
+include(string(path,"code/model.jl"));
 
+# Settings
 params = Dict("beta" => .99, "eta" => 0.032, "alpha" => .115, "delta" => .05, 
 	"g1" => .000177, "g2" =>.0044, "g31" => .000, "g32" => .039,  "g33" => .138);
 
-T = 20;
+# Uncertainty modeling
+T = 18;
+T1 = 3;
 Y = 10;
-D = 10;
+D = 5;
 
 # Running and plotting
+for f in ["random"]
+	for c in ["none", "average"]
 
-res1 = run_model(T, Y, D, .99, params, "Nordhaus");
+		res1 = run_model(T, T1, Y, D, .99, params, dmg="Nordhaus", clim=c, fut=f);
 
-res2 = run_model(T, Y, D, .99, params, "Medium");
-plot_model(res2, res1, scat=false)
+		res2 = run_model(T, T1, Y, D, .99, params, dmg="Medium", clim=c, fut=f);
+		plot_model(res2, res1, scat=false)
+		Plots.savefig(string(path,"output/outcome_Medium_",f,"_",c,".pdf"));
+		plot_model(res2, res1, scat=true)
+		Plots.savefig(string(path,"output/scatter_Medium_",f,"_",c,".pdf"));
 
-res3 = run_model(T, Y, D, .99, params, "Extreme");
-plot_model(res3, res1, scat=false)
+		res3 = run_model(T, T1, Y, D, .99, params, dmg="Extreme", clim=c, fut=f);
+		plot_model(res3, res1, scat=false)
+		Plots.savefig(string(path,"output/outcome_Extreme_",f,"_",c,".pdf"));
+		plot_model(res3, res1, scat=true)
+		Plots.savefig(string(path,"output/scatter_Extreme_",f,"_",c,".pdf"));
+		
+		res = run_model(T, T1, Y, D, .99, params, clim=c, fut=f);
+		plot_model(res, res1, scat=false)
+		Plots.savefig(string(path,"output/outcome_",f,"_",c,".pdf"));
+		plot_model(res, res1, scat=true)
+		Plots.savefig(string(path,"output/scatter_",f,"_",c,".pdf"));
 
-res = run_model(T, Y, D, .99, params, "none");
-plot_model(res, res1, scat=false)
+	end
+end
